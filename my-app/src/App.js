3b7ceb2f4cc178bcd21bcda6ee75e4cc1b7dac1d
Georgy <andreevgeorge@yandex.ru>
2021-12-02 12:37:54 +0100

    draft profile

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { BrowserRouter } from "react-router-dom";
+import Profile from './pages/Profile';
 import Routers from "./routes/Routers";
 
 function App() {
   return (
     <BrowserRouter>
-      <Routers />
+      <Profile />
     </BrowserRouter>
   );
 }
